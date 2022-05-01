b1e37ae361479571aa667b582acda603ebdf1976
Mees van Dijk <mees@mees.io>
2022-05-01 04:33:01 +0200

    Add automatic deploy script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+docker rmi derposbot:latest
+docker build -t derposbot:latest .
+docker save -o ./image.tar derposbot:latest
+gzip ./image.tar
+scp image.tar.gz mees@s.mees.io:derposbot.tar.gz
+rm image.tar.gz
+ssh mees@s.mees.io << EOF
+  docker rm -f derposbot
+  docker rmi derposbot:latest
+  gzip -d derposbot.tar.gz
+  docker load -i derposbot.tar
+  rm derposbot.tar
+  cd meesio
+  docker-compose up -d
+EOF
