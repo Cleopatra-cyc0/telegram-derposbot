2512f29bcc6267c2e4aea14fec86b12c14f5a8fe
Mees van Dijk <mees@mees.io>
2022-05-02 23:12:43 +0200

    Read loglevel from env

--- src/log.ts
+++ src/log.ts
@@ -1,7 +1,9 @@
 import Pino from "pino"
 import { hrtime } from "process"
 
-export default Pino()
+export default Pino({
+  level: process.env.LOG_LEVEL,
+})
 
 export const track = (): (() => { timeTaken: bigint }) => {
   const start = hrtime.bigint()
