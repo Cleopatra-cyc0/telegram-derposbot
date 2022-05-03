593bdfe41e4d0b6e6bead74c748d9c4ed9117df9
Mees van Dijk <mees@mees.io>
2022-05-03 21:35:52 +0200

    Improve debugging

--- src/log.ts
+++ src/log.ts
@@ -1,8 +1,21 @@
 import Pino from "pino"
 import { hrtime } from "process"
 
+const pinoTransport =
+  process.env.NODE_ENV === "development"
+    ? {
+        transport: {
+          target: "pino-pretty",
+          options: {
+            colorize: true,
+          },
+        },
+      }
+    : {}
+
 export default Pino({
   level: process.env.LOG_LEVEL,
+  ...pinoTransport,
 })
 
 export const track = (): (() => { timeTaken: bigint }) => {
