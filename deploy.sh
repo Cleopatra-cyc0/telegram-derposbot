8d4b010654055ad714bf3056841b0be67584c167
Mees van Dijk <mees@mees.io>
2022-05-03 12:21:29 +0200

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ gzip ./image.tar
 scp image.tar.gz mees@s.mees.io:derposbot.tar.gz
 rm image.tar.gz
 ssh mees@s.mees.io << EOF
-  docker rm -f derposbot &
+  docker rm -f derposbot
   docker rmi derposbot:latest &
   gzip -d derposbot.tar.gz
   docker load -i derposbot.tar
