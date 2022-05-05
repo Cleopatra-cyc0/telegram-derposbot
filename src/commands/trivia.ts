89ab5d21bc5fa163323703ada2db36e3ea30f3b5
Mees van Dijk <mees@mees.io>
2022-05-05 23:58:02 +0200

    Add raad and chance

--- src/commands/trivia.ts
+++ src/commands/trivia.ts
@@ -3,14 +3,37 @@ import { MyContext } from ".."
 
 export default function triviaCommands(bot: Telegraf<MyContext>) {
   bot.hears("lul", ctx => {
-    ctx.reply("zuigen dan")
+    return ctx.reply("zuigen dan")
   })
 
   bot.command("nerd", ctx => {
-    ctx.replyWithHTML(`Oh jij coole nerd\n<pre language="json">${JSON.stringify(ctx.message, null, 2)}</pre>`)
+    return ctx.replyWithHTML(`Oh jij coole nerd\n<pre language="json">${JSON.stringify(ctx.message, null, 2)}</pre>`)
   })
 
   bot.command("isdebaropen", ctx => {
-    ctx.reply("misschien")
+    return ctx.reply("misschien")
+  })
+
+  bot.command("kut bot", ctx => {
+    const random = Math.random()
+    if (random > 0.7) {
+      return ctx.reply(":(")
+    } else if (random > 0.2) {
+      return ctx.reply("Hou je bek man")
+    } else {
+      return ctx.reply("sorry")
+    }
+  })
+
+  bot.command("raad", async ctx => {
+    await ctx.reply("moeie imeelen")
+  })
+
+  bot.command("kopofmunt", async ctx => {
+    if (Math.random() > 0.49) {
+      await ctx.reply("kop")
+    } else {
+      await ctx.reply("munt")
+    }
   })
 }
