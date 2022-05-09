0aeefad0178de5eadaff50bf84c18d9b9979b7e4
Mees van Dijk <mees@mees.io>
2022-05-09 22:47:10 +0200

    Force private chat

--- src/commands/connect.ts
+++ src/commands/connect.ts
@@ -28,14 +28,18 @@ if (!congressusClientSecret) {
  */
 export function connectCommands(bot: Telegraf<MyTelegrafContext>) {
   bot.command("connect", async ctx => {
-    const user = await ctx.db.getRepository(User).findOrCreate(ctx.message.from.id)
-    user.congresssusOauthState = uuid4()
-    ctx.db.persist(user)
-    const link = `${congressusDomain}/oauth/authorize?response_type=code&client_id=${congressusClientId}&scope=openid&state=${user.congresssusOauthState}`
+    if (ctx.chat.type === "private") {
+      const user = await ctx.db.getRepository(User).findOrCreate(ctx.message.from.id)
+      user.congresssusOauthState = uuid4()
+      ctx.db.persist(user)
+      const link = `${congressusDomain}/oauth/authorize?response_type=code&client_id=${congressusClientId}&scope=openid&state=${user.congresssusOauthState}`
 
-    ctx.reply(`Click on [this link](${link}) and log in to congressus to connect your acocunt`, {
-      parse_mode: "MarkdownV2",
-    })
+      ctx.reply(`Click on [this link](${link}) and log in to congressus to connect your acocunt`, {
+        parse_mode: "MarkdownV2",
+      })
+    } else {
+      ctx.reply("moet je even prive doen piepo")
+    }
   })
 }
 
