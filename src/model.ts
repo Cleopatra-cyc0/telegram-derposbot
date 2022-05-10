5b7e31993da036d0f909d21bedf5a22b6aa0b234
Mees van Dijk <mees@mees.io>
2022-05-10 03:06:35 +0200

    remove extra spaces 2

--- src/model.ts
+++ src/model.ts
@@ -88,7 +88,9 @@ async function fetchBirthdayMembers() {
 
   const result = body
     .filter(m => m.show_almanac_date_of_birth && IsSameDate(m.date_of_birth))
-    .map(m => [m.first_name, m.primary_last_name_prefix, m.primary_last_name_main].join(" "))
+    .map(m =>
+      [m.first_name, m.primary_last_name_prefix, m.primary_last_name_main].filter(n => n != null && n !== "").join(" "),
+    )
   logger.debug(time(), "congressus: fetch-all-birthdays")
   return result
 }
