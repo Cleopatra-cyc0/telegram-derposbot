f5d0c4f77c875f48ae13e1172b8f2d216e3d0827
Mees van Dijk <mees@mees.io>
2022-06-27 12:29:21 +0200

    fix congressus member getting

--- src/model.ts
+++ src/model.ts
@@ -1,6 +1,6 @@
 import { ErrorType, IsSameDate, MyError } from "./util"
 import logger, { track } from "./log"
-import fetch, { FetchError } from "node-fetch"
+import fetch, { FetchError, RequestInit } from "node-fetch"
 import { DateTime } from "luxon"
 
 const congressusToken = process.env.CONGRESSUS_TOKEN
@@ -13,9 +13,7 @@ if (!congressusToken) {
 export type CongressusMember = {
   id: 0
   username: string
-  status_id: 0
-  status: string
-  email: string
+  // Status omitted
   gender: string
   initials: string
   given_name: string
@@ -31,6 +29,7 @@ export type CongressusMember = {
     city: string
     country: string
   }
+  email: string
   phone_mobile: {
     number_full: string
     number_full_MSISDN: string
@@ -77,17 +76,46 @@ export type CongressusMember = {
   payment_success_uri: string
   payment_start_uri: string
 }
+
+type CongressusPage = {
+  has_prev: boolean
+  prev_num: number
+  has_next: boolean
+  next_num: number
+  data: CongressusMember[]
+  total: number
+}
+
+async function getAllPages(baseUrl: URL, fetchOptions: RequestInit = {}): Promise<CongressusMember[]> {
+  const data = []
+
+  let pageNum = 1
+  while (true) {
+    const url = new URL(baseUrl.toString())
+    url.searchParams.set("page", pageNum.toString())
+    url.searchParams.set("page_size", "100")
+    const firstPage = await fetch(url, { ...fetchOptions })
+    const pageData = (await firstPage.json()) as CongressusPage
+    data.push(...pageData.data)
+    if (pageData.has_next) {
+      pageNum = pageData.next_num
+    } else {
+      break
+    }
+  }
+  return data
+}
+
 async function fetchBirthdayMembers() {
   const time = track()
   // get birthdays
-  const body = await fetch("https://api.congressus.nl/v20/members", {
+  const allMembers = await getAllPages(new URL("/v30/members", "https://api.congressus.nl"), {
     headers: {
-      Authorization: `Bearer:${congressusToken}`,
+      Authorization: `Bearer: ${congressusToken}`,
     },
-  }).then(res => res.json() as Promise<CongressusMember[]>)
-
-  const result = body
-    .filter(m => m.show_almanac_date_of_birth && IsSameDate(m.date_of_birth))
+  })
+  const result = allMembers
+    .filter(m => m.show_almanac_date_of_birth && m.date_of_birth != null && IsSameDate(m.date_of_birth))
     .map(m =>
       [m.first_name, m.primary_last_name_prefix, m.primary_last_name_main].filter(n => n != null && n !== "").join(" "),
     )
