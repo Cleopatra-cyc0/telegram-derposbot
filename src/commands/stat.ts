32375c068d43fe532284641f09babb78cd4801f4
Mees van Dijk <mees@mees.io>
2022-11-08 17:06:28 +0100

    Add day average to stats

--- src/commands/stat.ts
+++ src/commands/stat.ts
@@ -69,10 +69,11 @@ export default async function recordStat(
       const monthAgo = DateTime.now().minus({ month: 1 })
       const countWeek = stats.filter(stat => stat.date > weekStart).length
       const countMonth = stats.filter(stat => stat.date > monthAgo).length
+      const dayAvg = Math.round(count / (DateTime.now().diff(firstStat.date).as("days") + 1))
       await ctx.reply(
         `Al ${count} keer sinds ${firstStat.date.toLocaleString(
           DateTime.DATE_MED,
-        )}\n${countWeek} waren deze week\n${countMonth} waren de laatste maand`,
+        )}\n${countWeek} waren deze week\n${countMonth} waren de laatste maand\nGemiddeld ${dayAvg} per dag`,
       )
     } else {
       await ctx.reply("Ik ken jou helemaal niet, flikker op")
