901109c858a4424b5595cdd7f49078ab000f3adf
Mees van Dijk <mees@mees.io>
2022-12-13 18:10:08 +0100

    Fix chat command and remove mention
    Using one function for mention and command gave loads of type errors

--- src/commands/chatgpt.ts
+++ src/commands/chatgpt.ts
@@ -1,6 +1,7 @@
 import { Configuration, OpenAIApi } from "openai"
-import { Middleware, Telegraf } from "telegraf"
+import { Telegraf } from "telegraf"
 import { MyTelegrafContext } from ".."
+import logger from "../log"
 import { BotCommandScope, registerCommand } from "./commandlist"
 const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -11,22 +12,22 @@ const openai = new OpenAIApi(config)
 
 export default function chatGpt(bot: Telegraf<MyTelegrafContext>) {
   registerCommand("chat", "praat met de bot (engels)", [BotCommandScope.Private, BotCommandScope.Groups])
-  const chatCommand = async (ctx: MyTelegrafContext & { from: { id: number } | undefined }) => {
-    const { data } = await openai.createCompletion({
-      model: "babbage",
-      prompt: "This is a test",
-      temperature: 0.9,
-      best_of: 1,
-      user: `telegram user: ${ctx.from?.id.toString()}`,
-    })
-    if (data.choices.length > 0 && data.choices[0].text != null) {
-      ctx.reply(data.choices[0].text)
-    } else {
+  bot.command("chat", async ctx => {
+    try {
+      const { data } = await openai.createCompletion({
+        model: "babbage",
+        prompt: ctx.message.text.split(" ").slice(1).join(" "),
+        temperature: 0.5,
+        best_of: 1,
+      })
+      if (data.choices.length > 0 && data.choices[0].text != null) {
+        ctx.reply(data.choices[0].text)
+      } else {
+        throw new Error("No response from openapi")
+      }
+    } catch (e) {
+      logger.error(e, "Error in chatgpt")
       ctx.reply("Something went wrong")
     }
-  }
-  bot.command("chat", chatCommand)
-  if (bot.botInfo != null) {
-    bot.mention(bot.botInfo.username, chatCommand)
-  }
+  })
 }
