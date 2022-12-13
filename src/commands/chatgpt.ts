628af3c9f48a1521dd6ea2b5772a1656ded24b8e
Mees van Dijk <mees@mees.io>
2022-12-13 18:16:38 +0100

    Rate limit chat command

--- src/commands/chatgpt.ts
+++ src/commands/chatgpt.ts
@@ -2,6 +2,7 @@ import { Configuration, OpenAIApi } from "openai"
 import { Telegraf } from "telegraf"
 import { MyTelegrafContext } from ".."
 import logger from "../log"
+import { checkAndInsertRateLimit } from "../util"
 import { BotCommandScope, registerCommand } from "./commandlist"
 const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -9,25 +10,32 @@ const config = new Configuration({
 })
 
 const openai = new OpenAIApi(config)
-
+const superAdminUserId = parseInt(process.env.SUPER_ADMIN_USER_ID as string)
 export default function chatGpt(bot: Telegraf<MyTelegrafContext>) {
   registerCommand("chat", "praat met de bot (engels)", [BotCommandScope.Private, BotCommandScope.Groups])
   bot.command("chat", async ctx => {
-    try {
-      const { data } = await openai.createCompletion({
-        model: "babbage",
-        prompt: ctx.message.text.split(" ").slice(1).join(" "),
-        temperature: 0.5,
-        best_of: 1,
-      })
-      if (data.choices.length > 0 && data.choices[0].text != null) {
-        ctx.reply(data.choices[0].text)
-      } else {
-        throw new Error("No response from openapi")
+    if (checkAndInsertRateLimit(`chat ${ctx.from.id}`, 1, 60 * 60 * 1000) || ctx.from.id === superAdminUserId) {
+      try {
+        const { data } = await openai.createCompletion({
+          model: "babbage",
+          prompt: ctx.message.text.split(" ").slice(1).join(" "),
+          temperature: 0.5,
+          best_of: 1,
+        })
+        if (data.choices.length > 0 && data.choices[0].text != null) {
+          ctx.reply(data.choices[0].text)
+        } else {
+          throw new Error("No response from openapi")
+        }
+      } catch (e) {
+        logger.error(e, "Error in chatgpt")
+        ctx.reply("Something went wrong")
       }
-    } catch (e) {
-      logger.error(e, "Error in chatgpt")
-      ctx.reply("Something went wrong")
+    } else {
+      ctx.reply(
+        `Omdat dit centjes kost mag je maar 1 keer per uur.
+        Vraag anders aan bestuur of ze willen betalen (kost *bijna* niks)`,
+      )
     }
   })
 }
